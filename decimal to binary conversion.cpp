355236fa6d735125e9104c8a826cb35b27266a9e
SanjanaSinghChauhan <44131240+SanjanaSinghChauhan@users.noreply.github.com>
2019-10-05 22:06:29 +0530

    Add files via upload

---
+++ decimal to binary conversion.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+}*top = NULL, *p = NULL, *np = NULL;
+int x;
+void push(int n)
+{
+    np = new node;
+    np->data = n;
+    np->next = NULL;
+    if (top == NULL)
+    {
+        top = np;
+    }
+    else
+    {
+        np->next = top;
+        top = np;
+    }
+}
+int pop()
+{
+    if (top == NULL)
+    {
+        cout<<"underflow\n";
+    }
+    else
+    {
+        p = top;
+        top = top->next;
+        x = p->data;
+        delete(p);
+        return(x);
+    }
+}
+int main()
+{
+    int n, a;
+    cout<<"Enter the Decimal Number\n";
+    cin>>n;
+    while (n > 0)
+    {
+        a = n % 2;
+        n = n / 2;
+        push(a);
+    }
+    p = top;
+    cout<<"resultant binary no:";
+    while(true)
+    {
+        if (top != NULL)
+	    cout<<pop()<<"\t";
+	else
+	    break;
+    }
+}
